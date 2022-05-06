c460a57c939dfe97c75127c1e2cf3494d78cf9bf
CyskoFR <franc.sko91@gmail.com>
2022-05-06 18:33:08 +0200

    add palindroma

--- palindroma/js/main.js
+++ palindroma/js/main.js
@@ -1,2 +1,20 @@
 // Chiedere all’utente di inserire una parola
 // Creare una funzione per capire se la parola inserita è palindroma
+
+const word = prompt("Inserisci una parola");
+
+function checkPalindrome(word) {
+    let reverseWord = "";
+    for (let i = word.length - 1; i >= 0; i--) {
+        reverseWord += word[i];
+    }
+    return reverseWord;
+}
+
+console.log(checkPalindrome(word));
+
+if (word === checkPalindrome(word)) {
+    alert(`"${word}" è una parola palindroma!`);
+} else {
+    alert(`"${word}" non è una parola palindroma`);
+}
